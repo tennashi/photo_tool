65123d96780369e705f8c05260a8cd9144c7fc95
大田勇也 <ota@mac-marv.local>
2015-03-27 09:57:11 +0900

    update photo.rb

--- photo.rb
+++ photo.rb
@@ -8,4 +8,65 @@ require 'pathname'
 require 'kconv'
 require 'optparse'
 require 'exifr'
+require 'RMagick'
+
+# 写真を一括処理するクラス
+class PhotoDir < Dir
+    def initialize(dir = ./)
+        @photo_dir = dir if dir.exists?
+    end
+
+    # 名前を配列に保管
+    def get_name
+        names = Array.new
+        @photo_dir.each do |name|
+            names << name
+        end
+        return names
+    end
+
+    # 日付を配列に保管
+    def get_date
+        dates = Array.new
+        # 時間を 0324123515 のように取得する
+        @photo_dir.get_name.each do |name|
+            dates << EXIFR::JPEG::new(name).date_time.strftime("%m%d%H%M%S")
+        end
+        return dates
+    end
+
+    def get_hash_sorted_by_date!
+        names = @photo_dir.get_name
+        dates = @photo_dir.get_date
+        # 2つの配列から Hash を作る
+        photos = Hash[names.zip dates]
+        # Hash を 値でソート
+        sorted = photos.sort_by {|key, value| value}
+        return sorted
+    end
+
+    def rotate
+        @photo_dir.get_name.each do |name|
+            image = File.open(name).read
+            m_image = Magick::Image.from_blob(image).shift
+            m_image.auto_orient!
+        end
+    end
+
+    def resize(height, width)
+        @photo_dir.get_name.each do |name|
+            image = File.open(name).read
+            m_image = Magick::Image.from_blob(image).shift
+            m_image.resize!(height, width)
+        end
+    end
+
+    def rename_by_date
+        i = 1
+        @photo_dir.get_hash_sorted_by_date!.each_key do |key|
+            File.rename(key, sprintf("%10.4d", i) + ".jpg")
+            i += 1
+        end
+    end
+end
 
--- photo_publisher.rb
+++ photo_publisher.rb
@@ -10,8 +10,6 @@ require 'RMagick'
 
 class PhotoPublisher
     def initialize
-        @photo_dir
-        @photo_date
         @date
         @event_name
         @opts = OptionParser.new
@@ -20,48 +18,10 @@ class PhotoPublisher
 
     def initialize_parser
         @opts.on('-d', '--directory=PHOTO_DIR', "set a photo directory") do |dir|
-            @photo_dir = dir
         end
-    end
-
-    def get_photo_names(photo_dir)
-        dir = Dir.open("photo_dir")
-        photo_names = Array.new
-        dir.each do |name|
-            photo_names << name
+        @opts.on('-h', '--help', TrueClass, "show a help message") do
         end
-        return photo_names
-    end
-
-    def should_rotate?(photo)
-    end
-
-    def rotate(photo)
-    end
-
-    def resize(width, height, photo)
-    end
-
-    def get_date(photo_names)
-        photos = Hash.new
-        photo_names.each do |name|
-            date = EXIFR::JPEG::new(name).date_time
-            photos[name] = date.strftime("%m%d%H%M%S")
-        end
-        return photos
-    end
-
-    def sort_by_date(photos)
-        sorted_photos = Hash.new
-        sorted_photos = photos.sort_by {|key, value| value}
-        return sorted_photos
-    end
-
-    def rename(sorted_photos)
-        i = 1
-        sorted_photos.each_key do |key|
-            File.rename(key, sprintf("%10.4d", i) + ".jpg")
-            i += 1
+        @opts.on('-u', '--user', "set user") do
         end
     end
 
